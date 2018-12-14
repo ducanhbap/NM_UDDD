3c146bd2e9c647a030ffbf58c4220912263e1c4e
Hương Thoa <thoa@Thoas-MacBook-Pro.local>
2018-12-14 14:55:36 +0700

    màn hình câu nói

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Hương Thoa on 11/11/18.
+//  Copyright © 2018 Hương Thoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: UI Elments
+    @IBOutlet weak var lblQuote: UILabel!
+    @IBOutlet weak var lblAuthors: UILabel!
+    @IBOutlet weak var imgBackGround: UIImageView!
+    
+    // MARK: Data model
+    var quotes = [String]()
+    var authors = [String]()
+    var selectedIndex: Int = -1
+    
+    // MARK: Events
+    @IBAction func btnAction_Tapped(_ sender: UIButton) {
+
+        var newIndex = -1
+        
+        repeat{
+            newIndex = Int(arc4random_uniform(UInt32(quotes.count)))
+        }
+        while(newIndex == selectedIndex)
+        
+        selectedIndex = newIndex
+        
+        lblQuote.text = quotes[selectedIndex]
+        lblAuthors.text=authors[selectedIndex]
+        
+        imgBackGround.image=UIImage(named: "bg0\(selectedIndex).png")
+        
+        print(selectedIndex)
+    }
+    
+    
+    // MARK: Xử lí dành cho ViewControler riêng
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        quotes.append("Life is 10% what happens to you and 90% how you react to it")
+        quotes.append("Find three hobbies you love: one to make you money, one to keep you in shape, and one to be creative")
+        quotes.append("In order to succeed, we must first believe than we can")
+        quotes.append("There is nothing impossible to him who will try")
+        quotes.append("Over-thinking ruins you. Ruins the situation, twists things around, makes you worry and just makes everything much worse than it actually is")
+        quotes.append("Practice like you've never won. Perform like you've never lost")
+        
+        authors=["Charles R.Swindoll","","Nikos Karantrakis","Alexander The Great","No Name","No Name","No Name"]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+
+
+}
